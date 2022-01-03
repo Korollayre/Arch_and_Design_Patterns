0f02f4272df59fd967184cdf435f0c4358eb0efa
Mikhail <mbuinichienko@mail.ru>
2022-01-03 17:28:48 +0300

    Add logic to work with GetRequests and PostRequests classes

--- roach_framework/main.py
+++ roach_framework/main.py
@@ -1,3 +1,7 @@
+from quopri import decodestring
+from requests import GetRequests, PostRequests
+
+
 class PageNotFound404Error:
     """
     Класс - исключение для обработки ошибки вызова несуществующей страницы.
@@ -11,6 +15,7 @@ class Framework:
     """
     Класс - основная логика работы фреймворка
     """
+
     def __init__(self, urls, middlewares):
         self.urls_lst = urls
         self.middlewares_lst = middlewares
@@ -21,12 +26,23 @@ class Framework:
         if not url_path.endswith('/'):
             url_path = f'{url_path}/'
 
+        request = {}
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+            print(f'Нам пришли GET-параметры: {request["request_params"]}')
+        if method == 'POST':
+            data = PostRequests().get_request_data(environ)
+            request['data'] = Framework.decode_value(data)
+            print(f'Нам пришёл post-запрос: {request["data"]}')
+
         if url_path in self.urls_lst:
             view = self.urls_lst[url_path]
         else:
             view = PageNotFound404Error()
-
-        request = {}
 
         for middleware in self.middlewares_lst:
             middleware(request)
@@ -34,3 +50,12 @@ class Framework:
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
+
+    @staticmethod
+    def decode_value(data):
+        new_data = {}
+        for k, v in data.items():
+            val = bytes(v.replace('%', '=').replace("+", " "), 'UTF-8')
+            val_decode_str = decodestring(val).decode('UTF-8')
+            new_data[k] = val_decode_str
+        return new_data
