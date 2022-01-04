0cdf088590539eb8899f292b6d3c8513e4c2e15e
Mikhail <mbuinichienko@mail.ru>
2022-01-04 12:08:03 +0300

    Added titles for all controllers

--- views.py
+++ views.py
@@ -3,14 +3,14 @@ from roach_framework.temlator import render
 
 class Index:
     def __call__(self, request):
-        return '200 OK', render('index.html')
+        return '200 OK', render('index.html', title='Welcome')
 
 
 class About:
     def __call__(self, request):
-        return '200 OK', render('about.html')
+        return '200 OK', render('about.html', title='About Us')
 
 
 class Contacts:
     def __call__(self, request):
-        return '200 OK', render('contact.html')
+        return '200 OK', render('contact.html', title='Contacts')
