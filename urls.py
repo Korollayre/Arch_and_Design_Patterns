b6a4e0a51d333e583293fb795e3c8692e36a6cb5
Korollayre <mbuinichienko@mail.ru>
2022-01-18 15:14:20 +0300

    Delete 'routes' from urls.py file

--- urls.py
+++ urls.py
@@ -1,19 +1,5 @@
-from views import Index, Store, About, Contacts, CategoryCreate, GameCreate, GameCopy
-
-
 def other_front(request):
     request['key'] = 'value'
 
 
-fronts = (other_front,)
-
-routes = {
-    '/': Index(),
-    '/index/': Index(),
-    '/store/': Store(),
-    '/create-category/': CategoryCreate(),
-    '/create-game/': GameCreate(),
-    '/copy-game/': GameCopy(),
-    '/about/': About(),
-    '/contacts/': Contacts(),
-}
+FRONTS = (other_front,)
