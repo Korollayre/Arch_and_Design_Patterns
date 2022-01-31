9028cd3007fff78acb0a409a3ddf0fcf9895e668
Korollayre <mbuinichienko@mail.ru>
2022-01-31 15:06:54 +0300

    Add logic to work with DB

--- pattenrs/creational_patterns.py
+++ pattenrs/creational_patterns.py
@@ -1,10 +1,11 @@
+import itertools
 from copy import deepcopy
 from sqlite3 import connect
 from quopri import decodestring
 
-from errors import *
+from pattenrs.errors import *
 from pattenrs.behavioral_patterns import ConsoleWriter, Subject
-from unit_of_work import DomainObject
+from pattenrs.unit_of_work import DomainObject
 
 CONNECTION = connect('db.sqlite')
 
@@ -59,24 +60,21 @@ class Game(GamePrototype, DomainObject):
     """
     Класс - общий интерфейс игр.
     """
+    auto_id = itertools.count(1)
 
-    def __init__(self, name, description, price, release_date, categories):
+    def __init__(self, name, description, price, release_date):
+        self.id = next(Game.auto_id)
         self.name = name
         self.description = description
         self.price = price
         self.release_date = release_date
-        self.categories = categories
-
-        for category in categories:
-            category.games.append(self)
-            category.notify()
 
 
 class GameMapper:
     def __init__(self, conn):
         self.connection = conn
         self.cursor = self.connection.cursor()
-        self.table_name = 'Games'
+        self.table_name = 'games'
 
     def all(self):
         statement = f'SELECT * from {self.table_name}'
@@ -90,14 +88,11 @@ class GameMapper:
 
     def find_by_id(self, obj_id):
         statement = f'SELECT * FROM {self.table_name} WHERE id = ?'
-        self.cursor.execute(statement, (obj_id,))
+        self.cursor.execute(statement, (str(obj_id),))
         res = self.cursor.fetchone()
-        if res:
-            game = Game(*res[1:])
-            game.id = res[0]
-            return game
-        else:
-            raise RecordNotFoundException(f'Record with id={obj_id} not found')
+        game = Game(*res[1:])
+        game.id = res[0]
+        return game
 
     def insert(self, obj):
         statement = f'INSERT INTO {self.table_name} ' \
@@ -128,27 +123,23 @@ class GameMapper:
 
     def delete(self, obj_id):
         statement = f'DELETE FROM {self.table_name} WHERE id = ?'
-        self.cursor.execute(statement, (obj_id,))
+        self.cursor.execute(statement, (str(obj_id),))
         try:
             self.connection.commit()
         except Exception as e:
             raise DbDeleteException(e.args)
 
 
-class Category(Subject):
+class Category(Subject, DomainObject):
     """
     Класс-интерфейс категорий игр.
     """
+    auto_id = itertools.count(1)
 
-    def __init__(self, name, category):
+    def __init__(self, name):
+        self.id = next(Category.auto_id)
         self.name = name
-        self.main_category = category
-        self.sub_categories = []
-        self.games = []
         super().__init__()
-
-        if self.main_category:
-            self.main_category.sub_categories.append(self)
 
     def games_count(self):
         """
@@ -156,18 +147,27 @@ class Category(Subject):
 
         :return: Кол-во игр в категории.
         """
-        result = len(self.games)
-        if self.sub_categories:
-            for category in self.sub_categories:
-                result += category.games_count()
-        return result
+
+        games_categories_mapper = MapperRegistry.get_current_mapper('games_categories')
+        categories_dependence_mapper = MapperRegistry.get_current_mapper('categories_dependence')
+
+        games = games_categories_mapper.find_by_category_id(self.id)
+        res = len(games)
+
+        sub_categories = categories_dependence_mapper.find_by_main_category_id(self.id)
+        if sub_categories:
+            for sub_category in sub_categories:
+                sub_category_games = games_categories_mapper.find_by_category_id(sub_category.id)
+                res += len(sub_category_games)
+
+        return res
 
 
 class CategoryMapper:
     def __init__(self, conn):
         self.connection = conn
         self.cursor = self.connection.cursor()
-        self.table_name = 'Categories'
+        self.table_name = 'categories'
 
     def all(self):
         statement = f'SELECT * from {self.table_name}'
@@ -181,23 +181,16 @@ class CategoryMapper:
 
     def find_by_id(self, obj_id):
         statement = f'SELECT * FROM {self.table_name} WHERE id = ?'
-        self.cursor.execute(statement, (obj_id,))
+        self.cursor.execute(statement, (str(obj_id),))
         res = self.cursor.fetchone()
-        if res:
-            category = Category(*res[1:])
-            category.id = res[0]
-            return category
-        else:
-            raise RecordNotFoundException(f'Record with id={obj_id} not found')
+        category = Category(*res[1:])
+        category.id = res[0]
+        return category
 
     def insert(self, obj):
         statement = f'INSERT INTO {self.table_name} ' \
-                    f'(name, main_category, sub_categories, games) ' \
-                    f'VALUES (?,?,?,?)'
-        self.cursor.execute(statement, (obj.name,
-                                        obj.main_category,
-                                        obj.sub_categories,
-                                        obj.games))
+                    f'(name) VALUES (?)'
+        self.cursor.execute(statement, (obj.name,))
         try:
             self.connection.commit()
         except Exception as e:
@@ -205,12 +198,8 @@ class CategoryMapper:
 
     def update(self, obj):
         statement = f'UPDATE {self.table_name} ' \
-                    f'SET name = ?, main_category = ?, sub_categories = ?, games = ? ' \
-                    f'WHERE id = ?'
+                    f'SET name = ? WHERE id = ?'
         self.cursor.execute(statement, (obj.name,
-                                        obj.main_category,
-                                        obj.sub_categories,
-                                        obj.games,
                                         obj.id))
         try:
             self.connection.commit()
@@ -219,7 +208,122 @@ class CategoryMapper:
 
     def delete(self, obj_id):
         statement = f'DELETE FROM {self.table_name} WHERE id = ?'
-        self.cursor.execute(statement, (obj_id,))
+        self.cursor.execute(statement, (str(obj_id),))
+        try:
+            self.connection.commit()
+        except Exception as e:
+            raise DbDeleteException(e.args)
+
+
+class GamesCategoryModel(DomainObject):
+    def __init__(self, game_id, category_id):
+        self.game_id = game_id
+        self.category_id = category_id
+
+
+class GamesCategoryMapper:
+    def __init__(self, conn):
+        self.connection = conn
+        self.cursor = self.connection.cursor()
+        self.table_name = 'games_categories'
+
+    def all(self):
+        statement = f'SELECT * from {self.table_name}'
+        self.cursor.execute(statement)
+        res = []
+        for item in self.cursor.fetchall():
+            new_dict = dict()
+            new_dict['game_id'], new_dict['category_id'] = item[0], item[1]
+            res.append(new_dict)
+        return res
+
+    def find_by_category_id(self, obj_id):
+        statement = f'SELECT * FROM {self.table_name} WHERE category_id = ?'
+        self.cursor.execute(statement, (str(obj_id),))
+        rows = self.cursor.fetchall()
+        res = []
+        for item in rows:
+            mapper = MapperRegistry.get_current_mapper('game')
+            game = mapper.find_by_id(item[0])
+            res.append(game)
+        return res
+
+    def find_by_game_id(self, obj_id):
+        statement = f'SELECT * FROM {self.table_name} WHERE game_id = ?'
+        self.cursor.execute(statement, (str(obj_id),))
+        rows = self.cursor.fetchall()
+        res = []
+        for item in rows:
+            mapper = MapperRegistry.get_current_mapper('category')
+            category = mapper.find_by_id(item[1])
+            res.append(category)
+        return res
+
+    def insert(self, obj):
+        statement = f'INSERT INTO {self.table_name} ' \
+                    f'(game_id, category_id) ' \
+                    f'VALUES (?,?)'
+        self.cursor.execute(statement, (obj.game_id,
+                                        obj.category_id))
+        try:
+            self.connection.commit()
+        except Exception as e:
+            raise DbCommitException(e.args)
+
+    def delete(self, obj_id):
+        statement = f'DELETE FROM {self.table_name} WHERE id = ?'
+        self.cursor.execute(statement, (str(obj_id),))
+        try:
+            self.connection.commit()
+        except Exception as e:
+            raise DbDeleteException(e.args)
+
+
+class CategoriesDependenceModel(DomainObject):
+    def __init__(self, main_category_id, sub_category_id):
+        self.main_category_id = main_category_id
+        self.sub_category_id = sub_category_id
+
+
+class CategoriesDependenceMapper:
+    def __init__(self, conn):
+        self.connection = conn
+        self.cursor = self.connection.cursor()
+        self.table_name = 'categories_dependence'
+
+    def all(self):
+        statement = f'SELECT * from {self.table_name}'
+        self.cursor.execute(statement)
+        res = []
+        for item in self.cursor.fetchall():
+            res.append(item)
+        return res
+
+    def find_by_main_category_id(self, obj_id):
+        statement = f'SELECT * FROM {self.table_name} WHERE main_category_id = ?'
+        self.cursor.execute(statement, (str(obj_id),))
+        rows = self.cursor.fetchall()
+        res = []
+        for item in rows:
+            mapper = MapperRegistry.get_current_mapper('category')
+            category = mapper.find_by_id(item[1])
+            res.append(category)
+        return res
+
+    def insert(self, obj):
+        statement = f'INSERT INTO {self.table_name} ' \
+                    f'(main_category_id, sub_category_id) ' \
+                    f'VALUES (?,?)'
+        self.cursor.execute(statement, (obj.main_category_id,
+                                        obj.sub_category_id))
+        try:
+            self.connection.commit()
+        except Exception as e:
+            raise DbCommitException(e.args)
+
+    def delete(self, obj_id):
+        statement = f'DELETE FROM {self.table_name} WHERE id = ?'
+        self.cursor.execute(statement, (str(obj_id),))
         try:
             self.connection.commit()
         except Exception as e:
@@ -230,6 +334,8 @@ class MapperRegistry:
     mappers = {
         'game': GameMapper,
         'category': CategoryMapper,
+        'games_categories': GamesCategoryMapper,
+        'categories_dependence': CategoriesDependenceMapper,
     }
 
     @staticmethod
@@ -238,6 +344,10 @@ class MapperRegistry:
             return GameMapper(CONNECTION)
         elif isinstance(obj, Category):
             return CategoryMapper(CONNECTION)
+        elif isinstance(obj, GamesCategoryModel):
+            return GamesCategoryMapper(CONNECTION)
+        elif isinstance(obj, CategoriesDependenceModel):
+            return CategoriesDependenceMapper(CONNECTION)
 
     @staticmethod
     def get_current_mapper(name):
@@ -250,20 +360,31 @@ class Engine:
     """
 
     def __init__(self):
+        category_mapper = MapperRegistry.get_current_mapper('category')
+        games_mapper = MapperRegistry.get_current_mapper('game')
+        games_categories_mapper = MapperRegistry.get_current_mapper('games_categories')
+        categories_dependence_mapper = MapperRegistry.get_current_mapper('categories_dependence')
+
+        self.categories = category_mapper.all()
+        self.games = games_mapper.all()
+        self.games_categories = games_categories_mapper.all()
+        self.categories_dependence = categories_dependence_mapper.all()
         self.developers = []
         self.customers = []
-        self.games = []
-        self.categories = []
 
     @staticmethod
     def create_user(type_):
         return UserFactory.create(type_)
 
-    def create_category(self, name, main_category):
+    def create_category(self, name):
         for category in self.categories:
             if name == category.name:
                 raise ValueError(f'Категория с таким именем уже существует. Отмена операции.')
-        return Category(name, main_category)
+        return Category(name)
+
+    @staticmethod
+    def create_category_dependence(main_category_id, sub_category_id):
+        return CategoriesDependenceModel(main_category_id, sub_category_id)
 
     def find_category_by_id(self, request_id):
         """
@@ -278,11 +399,11 @@ class Engine:
                 return item
         raise ValueError(f'Категория с id = {request_id} отсутствует в базе данных.')
 
-    def create_game(self, name, description, price, release_date, categories):
+    def create_game(self, name, description, price, release_date):
         for game in self.games:
             if name == game.name:
                 raise ValueError(f'Игра с таким именем уже существует. Отмена операции.')
-        return Game(name, description, price, release_date, categories)
+        return Game(name, description, price, release_date)
 
     def get_game(self, name):
         """
@@ -296,6 +417,10 @@ class Engine:
             if item.name == name:
                 return item
         return None
+
+    @staticmethod
+    def create_game_category(game_id, category_id):
+        return GamesCategoryModel(game_id, category_id)
 
     @staticmethod
     def decode_value(val):
